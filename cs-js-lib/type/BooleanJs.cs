0086d61b673ea6c8598ddcca7f6681bd3c16bc53
Subhajit Sahu <wolfram77@gmail.com>
2016-03-12 00:02:34 +0530

    created boolean class

---
+++ cs-js-lib/type/BooleanJs.cs
@@ -0,0 +1,68 @@
+﻿/// BOOLEAN - The Boolean object is an object wrapper for a boolean value
+using System;
+
+namespace cs_js_lib.type {
+
+	/// <summary>The Boolean object is an object wrapper for a boolean value.</summary>
+	struct BooleanJs {
+
+		#region data
+		/// <summary>private: boolean data</summary>
+		private bool o;
+		#endregion
+
+
+		#region constructor
+		/// <summary>Creates a Boolean wrapper object for a boolean value</summary>
+		/// <param name="value">Optional. The initial value of the Boolean object.</param>
+		public BooleanJs(object value) {
+			o = value != null ? true : false;
+		}
+		/// <summary>Creates a Boolean wrapper object for a boolean value</summary>
+		/// <param name="value">Optional. The initial value of the Boolean object.</param>
+		public BooleanJs(string value) {
+			o = value.Length > 0 ? true : false;
+		}
+		/// <summary>Creates a Boolean wrapper object for a boolean value</summary>
+		/// <param name="value">Optional. The initial value of the Boolean object.</param>
+		public BooleanJs (double value) {
+			o = value != 0 ? true : false;
+		}
+		/// <summary>Creates a Boolean wrapper object for a boolean value</summary>
+		/// <param name="value">Optional. The initial value of the Boolean object.</param>
+		public BooleanJs(long value) {
+			o = value != 0 ? true : false;
+		}
+		/// <summary>Creates a Boolean wrapper object for a boolean value</summary>
+		/// <param name="value">Optional. The initial value of the Boolean object.</param>
+		public BooleanJs(bool value=false) {
+			o = value;
+		}
+		#endregion
+
+
+		#region convert
+		/// <summary>Implicitly converts Boolean object to primitive boolean type</summary>
+		/// <param name="value">Boolean object</param>
+		public static implicit operator bool(BooleanJs value) {
+			return value.o;
+		}
+		#endregion
+
+
+		#region method
+		/// <summary>Converts a boolean value to a string, and returns the result</summary>
+		/// <returns>A String, either "true" or "false"</returns>
+		public override String ToString() {
+			return o ? "true" : "false";
+		}
+
+
+		/// <summary>Returns the primitive value of a boolean</summary>
+		/// <returns>	A Boolean, either "true" or "false"</returns>
+		public bool ValueOf() {
+			return o;
+		}
+		#endregion
+	}
+}
---
+++ cs-js-lib/type/MathJs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace cs_js_lib.type {
+	class MathJs {
+
+		/// <summary>Returns Euler's number (approx. 2.718)</summary>
+		public static readonly double E = Math.E;
+		/// <summary>Returns the natural logarithm of 2 (approx. 0.693)</summary>
+		public static readonly double LN2 = Math.Log(2);
+		/// <summary>Returns the natural logarithm of 10 (approx. 2.302)</summary>
+		public static readonly double LN10 = Math.Log(10);
+		/// <summary>Returns the base-2 logarithm of E (approx. 1.442)</summary>
+		public static readonly double LOG2E = 1 / Math.Log(2);
+		/// <summary>Returns the base-10 logarithm of E (approx. 0.434)</summary>
+		public static readonly double LOG10E = 1 / Math.Log(10);
+		/// <summary>Returns PI (approx. 3.14)</summary>
+		public static readonly double PI = Math.PI;
+		/// <summary>Returns the square root of 1/2 (approx. 0.707)</summary>
+		public static readonly double SQRT1_2 = 1 / Math.Sqrt(2);
+		/// <summary>Returns the square root of 2 (approx. 1.414)</summary>
+		public static readonly double SQRT2 = Math.Sqrt(2);
+
+
+		/// <summary>Returns the absolute value of x</summary>
+		/// <param name="x">Required. A number</param>
+		/// <returns>A Number, representing the absolute value of the specified number, or NaN if the value is not a number, or 0 if the value is null</returns>
+		public static double Abs(double x) { return Math.Abs(x); }
+
+	}
+}
